3106694d06d68d7757a8997e6f2f89d16de290c7
javier <francisco.rodriguez@magnothecnology.com>
2022-06-22 03:37:25 -0500

    Añadiendo propiedades para mostrar imagen

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,15 +1,32 @@
 <template>
 <div class="pokemon-container">
-  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" class="hidden-pokemon" alt="Pokemon">
+  <img :src="imgSrc" class="hidden-pokemon" alt="Pokemon">
   
-   <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" class="fade-in" alt="Pokemon">
+   <img v-if="showPokemon" :src="imgSrc" class="fade-in" alt="Pokemon">
 </div>
 
 </template>
 
 <script>
-export default {
 
+//Number, required
+export default {
+    props:{
+        pokemonId:{
+            type: Number,
+            required: true
+        },
+        showPokemon: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    computed: {
+        imgSrc(){
+            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`
+        }
+    }
 }
 </script>
 
